9cdc60dbae7134cfe084594e18447a0ab4edcf9f
Volodymyr Zakalyk <deohayer@mail.com>
2024-08-03 18:18:28 +0300

    GH-32: Add tests for uw uwf

---
+++ test/test_uw_uwf.py
@@ -0,0 +1,36 @@
+from .utilities import *
+
+WS=f"{HOME}/ws_uw"
+CMD="uwf"
+HELP=str(
+    "uw uwf [TGT...]\n"
+    "\n"
+    "Fetch the software.\n"
+    "\n"
+    "Positional arguments:\n"
+    "  TGT    Targets for the operation.\n"
+    "\n"
+    "Optional arguments:\n"
+    "  --help       Print the help text, then exit.\n"
+    "  --version    Print the version, then exit.\n"
+    "  --daemon     Launch in background.\n"
+)
+
+class Test:
+    def setup_method(self):
+        sh(f"uw init {WS}")
+
+    def teardown_method(self):
+        sh(f"rm -r {WS}")
+
+    def test_0000(self):
+        # --version
+        assert sh(f"cd {WS} && uw {CMD} --version", cap=True).out == VERSION
+
+    def test_0001(self):
+        # --help
+        assert sh(f"cd {WS} && uw {CMD} --help", cap=True).out == HELP
+
+    def test_0002(self):
+        # Default case.
+        assert sh(f"cd {WS} && uw {CMD}", cap=True, chk=False).out == "Not implemented.\n"
